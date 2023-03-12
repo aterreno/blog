7baf8c41b15826df04b4996fa8eb20dd0b7fc194
Antonio Terreno <aterreno@users.noreply.github.com>
2023-03-12 09:49:20 +0000

    Updated Job & Social Links

--- config.js
+++ config.js
@@ -2,24 +2,24 @@ module.exports = {
   // the name of your website
   title: 'Antonio Terreno',
   // the description of the website (eg. what shows on Google)
-  description: "Antonio Terreno's blog",
+  description: "Antonio Terreno's blog archive",
   // a short bio shown at the bottom of your blog posts
   // It should complete the sentence: Written by Antonio Terreno ...
   shortBio: '',
   // a longer bio showing on the landing page of the blog
-  bio: `VP of Engineering @pharmaspectra.`,
+  bio: `Principal Consultant at Equal Experts`,
   author: 'Antonio Terreno',
   githubUrl: 'https://github.com/aterreno/blog',
   // replace this by the url where your website will be published
-  siteUrl: 'https://www.aterreno.com/',
+  siteUrl: 'https://javame.netlify.app/',
   social: {
     // leave the social media you do not want to appear as empty strings
     twitter: 'javame',
     medium: '',
     facebook: '',
     github: 'aterreno',
-    linkedin: 'aterreno',
-    instagram: '',
+    linkedin: 'antonioterreno',
+    instagram: 'tritonitri',
   },
   // GoatCounter code to enable analytics. See https://github.com/mathieudutour/medium-to-own-blog/tree/master/docs#analytics-tracking-page-views-etc
   goatCounterCode: null,
